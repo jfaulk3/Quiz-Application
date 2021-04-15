0110f533ff4da17060ae7d5dc4bdb7baf1b1e4d7
jfaulk3 <faulknerjimmie@gmail.com>
2021-04-15 13:51:13 -0500

    Redid buttons to make less confusing

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,124 @@
 import React, { useState } from "react";
 import Header from "./components/Header";
 import OptionButton from "./components/OptionButton";
-import { fetchCategories } from "./requests/util";
 
-const initialState: Options = { numQuestions: 10, allCategories: {} };
+const initialOptionValue = {
+  numQuestions: 0,
+  numCategories: 0,
+  numDifficulty: 0,
+  numTypes: 0,
+};
 
-interface Options {
-  numQuestions: number;
-  allCategories: object;
-}
+const initialAllOptions = {
+  categories: [],
+  difficulty: ["", "Easy", "Medium", "Hard"],
+  types: ["", "Multiple Choice", "True/False"],
+};
 
 function App() {
-  const [gameStart, setGameStart] = useState(true);
-  const [allOptions, setAllOptions]: [
-    Options,
-    (allOptions: Options) => void
-  ] = useState(initialState);
+  const [isGameStart, setIsGameStart]: [
+    boolean,
+    (isGameStart: boolean) => void
+  ] = useState<boolean>(false);
 
-  const changeAllOptions = (key: string, value: Object) => {
+  const [allOptions, setAllOptions]: [
+    any,
+    (allOptions: any) => void
+  ] = useState({ ...initialAllOptions });
+
+  const [optionVal, setOptionVal]: [any, (optionVal: any) => void] = useState({
+    ...initialOptionValue,
+  });
+
+  const changeOptionVal = (key: string, value: number) => {
+    setOptionVal({
+      ...optionVal,
+      [key]: value,
+    });
+  };
+
+  const changeOptionArray = (key: string, value: Array<string>) => {
     setAllOptions({
       ...allOptions,
       [key]: value,
     });
   };
 
-  const reset = () => {
-    setGameStart(false);
-    console.log("App-file: Home button pressed : Restart Beginning");
-  };
-
   React.useEffect(() => {
-    //TODO: get questions
-    fetchCategories(changeAllOptions);
+    const abortController = new AbortController();
+    const fetchData = async (url: string) => {
+      try {
+        //Will return an object that points to an array of values from opentdb API
+        const response = await fetch(url, { signal: abortController.signal });
+        const data = await response.json();
+        return data;
+      } catch (error) {
+        if (error.name === "AbortError") {
+          console.log("Aborted");
+        } else {
+          throw error;
+        }
+      }
+    };
+    fetchData("https://opentdb.com/api_category.php").then(
+      ({ trivia_categories }) => {
+        const array: Array<string> = trivia_categories.map(
+          ({ name }: { name: string }) => name
+        );
+        changeOptionArray("categories", ["", ...array]);
+      }
+    );
+    return () => abortController.abort();
   }, []);
 
-  console.log(allOptions);
+  const reset = () => {
+    setIsGameStart(false);
+    console.log("App-file: Home button pressed : Restart Beginning");
+  };
 
   return (
     <React.Fragment>
       <Header reset={reset} />
-      <OptionButton gameStart={gameStart} />
+      <OptionButton
+        isGameStart={isGameStart}
+        name={"Number of Questions: "}
+        type={"numQuestions"}
+        options={optionVal.numQuestions}
+        value={optionVal.numQuestions}
+        changeOptionVal={changeOptionVal}
+        minValueAllowed={1}
+        maxValueAllowed={50}
+      />
+      <OptionButton
+        isGameStart={isGameStart}
+        name={"Select a Category: "}
+        type={"numCategories"}
+        options={allOptions.categories}
+        value={optionVal.numCategories}
+        changeOptionVal={changeOptionVal}
+        minValueAllowed={0}
+        maxValueAllowed={allOptions.categories.length - 1}
+      />
+      <OptionButton
+        isGameStart={isGameStart}
+        name={"Select a Difficult: "}
+        type={"numDifficulty"}
+        options={allOptions.difficulty}
+        value={optionVal.numDifficulty}
+        changeOptionVal={changeOptionVal}
+        minValueAllowed={0}
+        maxValueAllowed={allOptions.difficulty.length - 1}
+      />
+      <OptionButton
+        isGameStart={isGameStart}
+        name={"Select Question Type: "}
+        type={"numTypes"}
+        options={allOptions.types}
+        value={optionVal.numTypes}
+        changeOptionVal={changeOptionVal}
+        minValueAllowed={0}
+        maxValueAllowed={allOptions.types.length - 1}
+      />
     </React.Fragment>
   );
 }
--- src/components/OptionButton/index.tsx
+++ src/components/OptionButton/index.tsx
@@ -1,30 +1,54 @@
-const OptionButton = ({ gameStart }) => {
+interface Props {
+  isGameStart: boolean;
+  name: string;
+  type: string;
+  options: any;
+  value: number;
+  changeOptionVal: (key: string, value: number) => void;
+  minValueAllowed: number;
+  maxValueAllowed: number;
+}
+
+const OptionButton: React.FC<Props> = ({
+  isGameStart,
+  name,
+  type,
+  options,
+  value,
+  changeOptionVal,
+  minValueAllowed,
+  maxValueAllowed,
+}) => {
+  if (isGameStart) return null;
+  const optVal = type === "numQuestions" ? value : options[value];
+  const changeValue = (value: number) => {
+    return Math.max(Math.min(value, maxValueAllowed), minValueAllowed);
+  };
   return (
     <div className="row bg-secondary text-white text-left">
       <button
         type="button"
         className="col-3 btn"
         onClick={() => {
-          changeOption(type, value - 1);
+          changeOptionVal(type, changeValue(value - 1));
         }}
       >
         <i className="icon-chevron-sign-left"></i>
       </button>
       <h5 className="col-6 pt-2">
-        {name}: {value}
+        {name}: {optVal || "Any"}
       </h5>
       <button
         type="button"
         className="col-3 btn"
         onClick={() => {
-          changeOption(type, value + 1);
+          changeOptionVal(type, changeValue(value + 1));
         }}
       >
         <i className="icon-chevron-sign-right"></i>
       </button>
     </div>
   );
-  return null;
 };
 
 export default OptionButton;
